817e448a8b1b3b458bad427780f4646cafb6c380
antonmattsson <anton.matinpoika@gmail.com>
2017-11-26 03:44:31 +0200

    devpost

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
-# junction2017
+# Diabetes profiling
+
+
 Project of Team Locus for Junction 2017
 
-How to pull this:
-
-```
-git clone git@github.com:antonmattsson/junction2017.git
-```
-
-![sql_tables](sql_tables.png)
+For details, see project_description.pdf
---
+++ src/heatmap.R
@@ -0,0 +1,34 @@
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+# SETWD!!!
+path <- "../downloaded_data/drug_connections_c_labeled.csv"
+
+drug_con <- read.csv(path, header = F, stringsAsFactors = F)
+
+cols <- c("drug1", drug_con$V1)
+colnames(drug_con) <- cols
+
+drug_mat <- as.matrix(drug_con[,-1])
+hc <- hclust(dist(drug_mat))
+drug_mat <- drug_mat[hc$order,hc$order]
+
+hc_order <- colnames(drug_mat)[hc$order]
+
+drug_plot_clustered <- drug_con %>%
+  gather("drug2","overlap", -drug1) %>%
+  mutate(Dataset = 1,
+         drug1 = factor(drug1, levels = hc_order),
+         drug2 = factor(drug2, levels = hc_order)) 
+
+p <- ggplot(drug_plot, aes(x=drug1, y = drug2, fill = overlap)) +
+  geom_tile() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text.x = element_text(angle = 90))
+p <- ggplotly(p)
+ 
+ htmlwidgets::saveWidget(p, "heatmap.html")
+ 
